b81e411bbba3e7f79256d754e2c3db744a00880f
OOPSProject-restaurantmanagement <oopsprojectsem2@gmail.com>
2025-05-04 04:52:17 +0530

    After updating gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,5 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
