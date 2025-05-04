e4507dbc1bcda65cc213a2d27a9c0d33de229ced
OOPSProject-restaurantmanagement <oopsprojectsem2@gmail.com>
2025-05-05 04:56:32 +0530

    After adding cool animations

--- app/src/main/java/com/example/oops/Entry.java
+++ app/src/main/java/com/example/oops/Entry.java
@@ -3,6 +3,9 @@ package com.example.oops;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,8 @@ public class Entry extends AppCompatActivity {
 
     Button managementButton;
     Button coustemerButton;
+    ImageView image ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +32,24 @@ public class Entry extends AppCompatActivity {
             return insets;
         });
 
-
         managementButton = findViewById(R.id.button);
-
         coustemerButton = findViewById(R.id.button2);
+        image = findViewById(R.id.imageView2);
 
-
+        Animation slideleft = AnimationUtils.loadAnimation(this, R.anim.slide_in_left);
+        managementButton.post(() -> managementButton.startAnimation(slideleft));
+        Animation slideright = AnimationUtils.loadAnimation(this, R.anim.slide_in_right);
+        coustemerButton.post(() -> coustemerButton.startAnimation(slideright));
+        Animation fadein = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        image.post(() -> image.startAnimation(fadein));
         managementButton.setOnClickListener(v -> {
             Intent intent = new Intent(Entry.this, ManagementLogin.class);
             startActivity(intent);
         });
 
         coustemerButton.setOnClickListener(v -> {
-                    Intent intent = new Intent(Entry.this, CustomerDashboard.class);
-                    startActivity(intent);
-                }
-
-        );
+            Intent intent = new Intent(Entry.this, CustomerDashboard.class);
+            startActivity(intent);
+        });
     }
 }
